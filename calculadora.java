fc45c4b549882e3c859273dada04b5ce232275e3
PedroV <a_pedro.vigara.haro@iespablopiccaso.es>
2024-02-14 09:46:39 +0100

    Añado las funciones de multiplicar y dividir

--- calculadora.java
+++ calculadora.java
@@ -9,13 +9,19 @@ public class calculadora {
         int primerNumero = Integer.parseInt(sc.nextLine());
         System.out.println("Escribe el segundo número: ");
         int segundoNumero = Integer.parseInt(sc.nextLine());
-        System.out.println("Escribe la operación a realizar (suma, resta): ");
+        System.out.println("Escribe la operación a realizar (suma, resta, multiplicar, dividir): ");
         String funcionARealizar = sc.nextLine();
 
         if (funcionARealizar.matches("suma")) {
             System.out.println("La suma de " + primerNumero + " + " + segundoNumero + " es " + (primerNumero + segundoNumero));
         } else if (funcionARealizar.matches("resta")) {
             System.out.println("La resta de " + primerNumero + " - " + segundoNumero + " es " + (primerNumero - segundoNumero));
+        } else if (funcionARealizar.matches("multiplicar")) {
+            System.out.println("La multiplicación de " + primerNumero + " * " + segundoNumero + " es " + (primerNumero * segundoNumero));
+        } else if (funcionARealizar.matches("dividir")) {
+            System.out.println("La división de " + primerNumero + " / " + segundoNumero + " es " + (primerNumero / segundoNumero));
+        } else {
+            System.out.println("Error");
         }
     }
 }
